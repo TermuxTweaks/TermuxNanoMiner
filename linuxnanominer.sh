123b772d7394a2277fef5846073f26e5e57697c0
ShadyTrapShit <114740185+ShadyTrapShit@users.noreply.github.com>
2023-10-14 15:50:45 -0400

    Update linuxnanominer.sh

--- linuxnanominer.sh
+++ linuxnanominer.sh
@@ -7,7 +7,7 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux
 tar -xzf xmrig-6.20.0-linux-x64.tar.gz
 
 # Enter the Xmrig directory
-cd xmrig-6.20.0-linux-x64
+cd xmrig-6.20.0
 
 # Run Xmrig
 ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XNO:nano_3uixbwndscgokp8g1tqwut7eyyjzaxkbug1j5ramf6zmjz7pppt8s4zyoj6a.unmineable_worker_zswfykx -p x
