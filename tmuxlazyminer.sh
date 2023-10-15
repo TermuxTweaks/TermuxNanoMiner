a615de9c9d4504e062ff7f9b829f17ba2fba53c9
ShadyTrapShit <114740185+ShadyTrapShit@users.noreply.github.com>
2023-10-15 15:54:08 -0400

    Update and rename tmuxnanominer.sh to tmuxlazyminer.sh

--- tmuxlazyminer.sh
+++ tmuxlazyminer.sh
@@ -14,5 +14,4 @@ cd build
 cmake -DWITH_HWLOC=OFF ..
 make
 
-# Run Xmrig
-./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XNO:nano_3uixbwndscgokp8g1tqwut7eyyjzaxkbug1j5ramf6zmjz7pppt8s4zyoj6a.unmineable_worker_zswfykx -p x
+
