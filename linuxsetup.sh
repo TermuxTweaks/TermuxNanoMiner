360f31539d129045f11ac2b66c04c1678ae982b3
ShadyTrapShit <114740185+ShadyTrapShit@users.noreply.github.com>
2023-10-15 15:28:38 -0400

    Update linuxsetup.sh

--- linuxsetup.sh
+++ linuxsetup.sh
@@ -1,3 +1,25 @@
-apt update && apt install git -y && git clone https://github.com/ShadyTrapShit/nanominer && cd nanominer && bash linuxnanominer.sh && cd xmrig-6.20.0 && ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XNO:nano_3uixbwndscgokp8g1tqwut7eyyjzaxkbug1j5ramf6zmjz7pppt8s4zyoj6a.unmineable_worker_zswfykx
+#!/bin/bash
+
+echo "Please choose a cryptocurrency: Bitcoin (B), Dogecoin (D), or Nano (N)."
+read -r choice
+
+case $choice in
+  B|b)
+    final_command="./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u BTC:bc1q0fzk4fgrl7dt5f8thkc8zvy8e6u604r8gq85tg.unmineable_worker_inhcado -p x"
+    ;;
+  D|d)
+    final_command="./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u DOGE:D6QUZViFSiEWkeAkGdYyPPoJszQ9RZBX29.unmineable_worker_qtdfydkv -p x"
+    ;;
+  N|n)
+    final_command="./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XNO:nano_3uixbwndscgokp8g1tqwut7eyyjzaxkbug1j5ramf6zmjz7pppt8s4zyoj6a.unmineable_worker_zswfykx"
+    ;;
+  *)
+    echo "Invalid choice. Exiting."
+    exit 1
+    ;;
+esac
+
+sudo apt update && sudo apt install git -y && git clone https://github.com/ShadyTrapShit/lazyminer && cd lazyminer && bash lazyminer.sh && cd xmrig-6.20.0 && $final_command
+
 
 
